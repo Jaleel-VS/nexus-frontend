dc8dec5a346824b671d5ff05fa238e3795bd23c6
Jaleel-VS <jeremikress@gmail.com>
2023-07-25 15:21:19 +0200

    implement services

--- backend/src/main/java/com/nexus/vouchers/services/InfluencerService.java
+++ backend/src/main/java/com/nexus/vouchers/services/InfluencerService.java
@@ -1,8 +1,9 @@
 package com.nexus.vouchers.services;
 
 import com.nexus.vouchers.models.Influencer;
+import com.nexus.vouchers.models.Voucher;
 import com.nexus.vouchers.models.VoucherRequest;
-import com.nexus.vouchers.repositories.InfluencerRepository;
+import com.nexus.vouchers.repositories.VoucherRepository;
 import com.nexus.vouchers.repositories.VoucherRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,19 +11,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class InfluencerService extends UserService<Influencer> {
     @Autowired
-    private InfluencerRepository influencerRepository;
+    VoucherRequestRepository voucherRequestRepository;
 
     @Autowired
-    VoucherRequestRepository voucherRequestRepository;
+    VoucherRepository voucherRepository;
 
     // request voucher
     public VoucherRequest requestVoucher(VoucherRequest voucherRequest) {
         return voucherRequestRepository.save(voucherRequest);
     }
 
-    // Get all voucher requests assigned to an influencer.
-    public Iterable<VoucherRequest> getVouchers(Long influencerId) {
+    // Get all voucher requests made by an influencer.
+    public Iterable<VoucherRequest> getVoucherRequests(Long influencerId) {
         return voucherRequestRepository.findAllByInfluencerId(influencerId);
+    }
+
+    // Get all voucher assigned to an influencer.
+    public Iterable<Voucher> getVouchers(Long influencerId) {
+        return voucherRepository.findAllByInfluencerId(influencerId);
+    }
+
+    // redeem a voucher at a supplier
+    public void redeemVoucher(Long voucherId, Long supplierId) {
+  //    TODO: implement
     }
 
 
