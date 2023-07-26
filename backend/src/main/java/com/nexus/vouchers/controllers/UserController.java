da32b754e6cac03d458be9bd91107ecfd58ae99d
Jaleel-VS <jeremikress@gmail.com>
2023-07-26 11:09:15 +0200

    update controllers

--- backend/src/main/java/com/nexus/vouchers/controllers/UserController.java
+++ backend/src/main/java/com/nexus/vouchers/controllers/UserController.java
@@ -1,13 +1,18 @@
+package com.nexus.vouchers.controllers;
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nexus.vouchers.models.User;
 import com.nexus.vouchers.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     @Autowired
-    private UserService userService;
+    private UserService<User> userService;
     
 }
