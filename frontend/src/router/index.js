00b53078e059807b262560986eff22cfe22689a2
Thaakirah Majiet <MJTTHA004@myuct.ac.za>
2023-07-26 14:52:10 +0200

    update router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,8 @@ import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import InfluencerPortal from '../views/InfluencerPortal.vue'
+import Influencer_Request_Voucher from '../views/Influencer_Request_Voucher.vue'
+import View_Vouchers from '../views/View_Vouchers.vue'
 
 const routes = [
   {
@@ -25,6 +27,17 @@ const routes = [
     path: '/influencer_portal',
     name: 'influencer_portal',
     component: InfluencerPortal
+  },
+
+  {
+    path: '/influencer_request_voucher',
+    name: 'influencer_request_voucher',
+    component: Influencer_Request_Voucher
+  },
+  {
+    path: '/view_vouchers',
+    name: 'view_vouchers',
+    component: View_Vouchers
   },
 ];
 
