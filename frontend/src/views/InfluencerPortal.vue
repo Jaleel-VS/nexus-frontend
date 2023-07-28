5ad69add88cb4da3c137e70a8a93b2fb5bb95569
Thaakirah Majiet <MJTTHA004@myuct.ac.za>
2023-07-28 14:39:15 +0200

    commit InfluencerPortal and view View_Vouchers

--- frontend/src/views/InfluencerPortal.vue
+++ frontend/src/views/InfluencerPortal.vue
@@ -2,72 +2,88 @@
 <template>
   <div>
     <img src="proj_logo.png" />
-  </div>
+    </div>
 
   <div class="InfluencerPortal">
     <h1 class="text-center">Influencer Portal</h1>
-
-    <div class="button-container">
-      <button v-on:click="Request_Voucher">Request Voucher</button>
-      <button v-on:click="View_Voucher">View Voucher</button>
-    </div>
-
+      <router-link to="/request_voucher">
+        <button class="cta-button">Request Voucher</button>
+      </router-link>
+      <router-link to="/view_voucher">
+        <button class="cta-button">View Voucher</button>
+      </router-link>
+      <router-link to="/">
+        <button class="logout-button">Log out</button>
+      </router-link>
   </div>
+
 </template>
 
-<script>
-export default {
-  name: "InfluencerPortal"
-}
-</script>
+  <script setup>
+  import { RouterLink } from 'vue-router';
+  </script>
 
-<style scoped>
-/* Your existing styles */
+  <style scoped>
 
-img {
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 400px;
-  height: auto;
-}
+img{
+            position: absolute;
+            top: 0;
+            left: 50%;
+            transform: translateX(-50%);
+            width: 400px;
+            height: auto;
 
-.InfluencerPortal {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  height: 100vh;
-  padding: 2em;
-  box-sizing: border-box;
-  font-family: Arial, sans-serif;
-  color: #333;
 }
+  .InfluencerPortal {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    height: 100vh;
+    padding: 2em;
+    box-sizing: border-box;
+    font-family: Arial, sans-serif;
+    color: #333;
+  }
 
-.InfluencerPortal h1 {
-  font-size: 2.5em;
-  margin-bottom: 25px;
-}
+  .InfluencerPortal h1 {
+    font-size: 2.5em;
+    margin-bottom: 1em;
+  }
 
-.button-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 20px; /* Adjust the top margin as desired */
-}
-
-.button-container button {
-  background-color: #3498db;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  padding: 1em 2em;
-  font-size: 1em;
-  text-decoration: none;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-  margin: 0 5px; /* Add margin between the buttons to create space */
-}
-</style>
+   .cta-button {
+    background-color: #3498db;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 1em 2em;
+    font-size: 1em;
+    text-decoration: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    margin-top: 20px;
+  }
+  
+  .cta-button:hover {
+    background-color: #2980b9;
+  }
+  .logout-button {
+    background-color: #3498db;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 1em 2em;
+    font-size: 1em;
+    text-decoration: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+  
+  .logout-button:hover {
+    background-color: #2980b9;
+  }
+  </style>
