5ceb99b8f6b91359b8e1eb7f5b59d8ce3b10145e
Jaleel-VS <jeremikress@gmail.com>
2023-07-31 07:48:34 +0200

    remove java backend

--- backend/src/main/java/com/nexus/vouchers/models/enums/RequestStatus.java
+++
@@ -1,7 +0,0 @@
-package com.nexus.vouchers.models.enums;
-
-public enum RequestStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
-}
--- backend/src/main/java/com/nexus/vouchers/models/enums/VoucherStatus.java
+++
@@ -1,7 +0,0 @@
-package com.nexus.vouchers.models.enums;
-
-public enum VoucherStatus {
-    CREATED,
-    REDEEMED,
-    EXPIRED
-}
