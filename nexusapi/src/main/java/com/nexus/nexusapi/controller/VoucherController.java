9f0ff26972ccc14298a6a7b1de06d7f1ef6dc576
Jaleel-VS <jeremikress@gmail.com>
2023-08-15 09:27:43 +0200

    update controller

--- nexusapi/src/main/java/com/nexus/nexusapi/controller/VoucherController.java
+++ nexusapi/src/main/java/com/nexus/nexusapi/controller/VoucherController.java
@@ -1,10 +1,58 @@
 package com.nexus.nexusapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import com.nexus.nexusapi.dto.response.VoucherResponseDTO;
+import com.nexus.nexusapi.service.VoucherService;
+import com.nexus.nexusapi.dto.request.VoucherDTO;
 
 @RestController
 @RequestMapping("/api/vouchers")
 public class VoucherController {
-    
+    @Autowired
+    private VoucherService voucherService;
+
+    // create voucher
+    @PostMapping
+    public ResponseEntity<VoucherDTO> createVoucher(VoucherDTO voucherDTO) {
+        return ResponseEntity.ok(voucherService.createVoucher(voucherDTO));
+    }
+
+    // get voucher by id
+    @GetMapping("/{id}")
+    public ResponseEntity<VoucherResponseDTO> getVoucherById(@PathVariable Long id) {
+        return ResponseEntity.ok(voucherService.getVoucherById(id));
+    }
+
+
+    // update voucher status
+    @PutMapping("/{id}/{status}")
+    public ResponseEntity<VoucherResponseDTO> updateVoucherStatus(@PathVariable Long id, @PathVariable boolean status) {
+        return ResponseEntity.ok(voucherService.updateVoucherStatus(id, status));
+    }
+
+    // check if pin matches
+    @GetMapping("/{id}/{pin}")
+    public ResponseEntity<Boolean> checkPin(@PathVariable Long id, @PathVariable String pin) {
+        return ResponseEntity.ok(voucherService.isPinMatch(id, pin));
+    }
+
+    // check if redeemed
+    @GetMapping("/{id}/redeemed")
+    public ResponseEntity<Boolean> isRedeemed(@PathVariable Long id) {
+        return ResponseEntity.ok(voucherService.isRedeemed(id));
+    }
+
+
+
+
+
 }
