d57d9658c5dff341cd88e99a7cd8705ee1a7cda4
vstjal001 <vstjal001@cs.uct.ac.za>
2023-09-04 09:00:19 +0200

    add image urls to models

--- nexusapi/src/main/java/com/nexus/nexusapi/model/Product.java
+++ nexusapi/src/main/java/com/nexus/nexusapi/model/Product.java
@@ -22,6 +22,7 @@ public class Product {
 
     private String productName;
     private String productDescription;
+    private String productImageUrl;
     private Double productPrice;
 
     @ManyToMany
