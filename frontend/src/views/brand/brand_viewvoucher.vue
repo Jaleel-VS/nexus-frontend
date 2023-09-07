c80c149cf6f939244b65af2feeb906b629d8ff78
savanne-alexander <sav.alexander1@gmail.com>
2023-09-07 14:24:03 +0200

    viewvoucher

--- frontend/src/views/brand/brand_viewvoucher.vue
+++ frontend/src/views/brand/brand_viewvoucher.vue
@@ -1,41 +1,33 @@
 <template>
     <div>
       <v-expansion-panels variant="popout" class="my-4">
-        <v-expansion-panel v-for="(voucher, index) in vouchers" :key="index">
-          <template v-slot:title>
-            <div class="panel-header">
-              Voucher {{ index + 1 }}
-              <button @click="toggleVoucherDetails(index)">
-                {{ voucherDetailsVisible[index] ? 'Hide Details' : 'Show Details' }}
-              </button>
-            </div>
-          </template>
-          <div v-if="voucherDetailsVisible[index]">
-            <div>
-                <p>Influencer: {{ voucher.influencer.name }}</p>
+        <v-expansion-panel v-for="(voucher, index) in vouchers" :key="index" :title="`Voucher ${index + 1}`">
+          <div>
+            
+            <p>Influencer: {{ voucher.influencer.name }}</p>
             <p>Brand: {{ voucher.brand.name }}</p>
             <p>Supplier: {{ voucher.supplier.name }}</p>
+            <button @click="toggleVoucherDetails(index)">Click here to see Voucher details</button> 
+            <div v-if="voucherDetailsVisible[index]"> 
             <p>Product: {{ voucher.product.name }}</p>
             <p>Created Date: {{ voucher.createdDate }}</p>
             <p>Expiry Date: {{ voucher.expiryDate }}</p>
             <p>Redeemed: {{ voucher.redeemed ? 'Yes' : 'No' }}</p>
             <p>Blockchain URL: {{ voucher.blockchainUrl }}</p>
-
             </div>
-            
           </div>
         </v-expansion-panel>
       </v-expansion-panels>
     </div>
-  </template>
-  
-  <script>
-  import { ref } from 'vue';
-  
-  export default {
-    setup() {
-      const vouchers = ref([
-      {
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const vouchers = ref([
+    {
     influencer: { name: 'Influencer 1' },
     brand: { name: 'Brand 1' },
     supplier: { name: 'Supplier 1' },
@@ -55,29 +47,41 @@
     redeemed: true,
     blockchainUrl: 'https://example.com/blockchain-url-2',
   },
-      ]);
+
+    ]);
+
+    const voucherDetailsVisible = ref(new Array(vouchers.value.length).fill(false));
+
+    function toggleVoucherDetails(index) {
+      voucherDetailsVisible[index] = !voucherDetailsVisible[index];
+    }
+    
+
+    return {
+      vouchers,
+      toggleVoucherDetails,
+      voucherDetailsVisible,
+    };
+  },
+};
+</script>
+
+
   
-      const voucherDetailsVisible = ref(new Array(vouchers.value.length).fill(false));
-  
-      function toggleVoucherDetails(index) {
-        voucherDetailsVisible[index] = !voucherDetailsVisible[index];
-      }
-  
-      return {
-        vouchers,
-        toggleVoucherDetails,
-        voucherDetailsVisible,
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .panel-header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  <style scoped lang="scss">
+
+.buttons {
+    display: flex;
+    gap: 20px;
 }
+
+.button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 1em;
+    transition: background-color 0.3s;
+}
+
   </style>
-  
-  
