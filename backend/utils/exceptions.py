a12f4c378e628b4f62f3862b0f58674d10c3d1e3
Jaleel <56233547+Jaleel-VS@users.noreply.github.com>
2023-09-17 11:17:31 +0200

    Delete backend directory

--- backend/utils/exceptions.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException
-
-class NotFoundError(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=404, detail=detail)
-
-class InternalServerError(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=500, detail=detail)
