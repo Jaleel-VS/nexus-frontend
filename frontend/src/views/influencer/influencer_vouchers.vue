f2ceaabe187277e6d46d33cf5164bd3125fa7414
Jaleel-VS <jeremikress@gmail.com>
2023-09-18 22:21:58 +0200

    father ariendal

--- frontend/src/views/influencer/influencer_vouchers.vue
+++ frontend/src/views/influencer/influencer_vouchers.vue
@@ -10,19 +10,49 @@
 
     <div v-else>
         <div v-if="voucherRequests && !selectedRequest" class="main-container">
-        <!-- <transition name="fade"> -->
-        <!-- <v-card
+        <v-card
           v-for="request in voucherRequests"
           class="mx-auto -card"
           min-width="200"
-        > -->
+        >
+
+        <v-img
+        class="align-end text-white"
+            height="200"
+            :src="getBrandDetails(request).profilePictureUrl"
+            cover
+            >
+        </v-img>
+        <v-card-text>
+            <div>ID: {{ request.id }}</div>
+            <div>Date: {{ request.requestDate }}</div>
+            <div>Status: {{ request.requestStatus }}</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" @click="selectRequest(request)">
+              Select
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        </div> 
+
+        <div v-if="selectedRequest" class="approve-container">
+        <h2>Viewing voucher request with id {{ selectedRequest.id }}</h2>
+
+        <button @click="claimVoucher">Claim</button>
         </div>
+
+
+
+
+
 
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref, onMounted } from "vue";
 import { API_ENDPOINT } from "@/config/constants.js";
 import { useUserStore } from "@/store/user";
@@ -50,11 +80,46 @@ onMounted(async () => {
 
 //   setBrandDetails
 
+for (let i = 0; i < voucherRequests.value.length; i++) {
+    const brandResponse = await fetch(
+      `${API_ENDPOINT}/users/brands/${voucherRequests.value[i].brandId}`
+    );
+
+    console.log(brandResponse);
+
+    if (brandResponse.status === 200) {
+      const brandData = await brandResponse.json();
+      brandDetails.value.push(brandData);
+    }
+  }
+
   
 
 
   loading.value = false;
 });
+
+const getBrandDetails = (request) => {
+
+    for (let i = 0; i < brandDetails.value.length; i++) {
+        if (brandDetails.value[i].id === request.brandId) {
+            return brandDetails.value[i];
+        }
+    }
+}
+
+const selectRequest = (request) => {
+  selectedRequest.value = request;
+};
+
+const deselectRequest = () => {
+  selectedRequest.value = null;
+};
+
+const claimVoucher = () => {
+  alert("Please supply the supplier with the voucherID to claim your product");
+  deselectRequest();
+}
 
 </script>
 
@@ -69,10 +134,12 @@ onMounted(async () => {
 
 .main-container {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   width: 100%;
+  flex-wrap: wrap;
+  gap: 2rem;
 }
 
 h1 {
@@ -114,4 +181,27 @@ h2 {
     transform: rotate(360deg);
   }
 }
+
+.approve-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  flex-wrap: wrap;
+}
+
+button {
+  background-color: #0b2c5c;
+  color: #fff;
+  font-size: 1.2em;
+  border-radius: 5px;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  justify-content: center;
+  display: flex;
+}
+
 </style>
